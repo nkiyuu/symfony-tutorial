c454e858577f5b3c899fa7eb77c5b834f649643d
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2019-10-28 21:18:37 +0900

    記事いい感じに表示するところまで

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Post;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/blog")
+*/
+class BlogController extends Controller
+{
+    /**
+     * @Route("/", name="blog_index")
+    */
+    public function indexAction(Request $request)
+    {
+        $em = $this ->getDoctrine()->getManager();
+        $posts = $em->getRepository(Post::class)->findAll();
+
+        return $this -> render('blog/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="blog_show")
+    */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository(Post::class)->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('The post does not exist');
+        }
+
+        return $this->render('blog/show.html.twig', ['post' => $post]);
+    }
+
+}
